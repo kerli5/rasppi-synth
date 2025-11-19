46ebd06fadeb5a2c6d3bc6b000568ac6c3342c56
Kerli Viitmaa <kerli5@tlu.ee>
2025-11-19 16:54:03 +0200

    audio player
    audio go sound

--- src/drumpad.py
+++ src/drumpad.py
@@ -1,13 +1,36 @@
 from PyQt6.QtWidgets import QWidget, QGridLayout, QPushButton, QMenu
+from PyQt6.QtMultimedia import QMediaPlayer, QAudioOutput
+from PyQt6.QtCore import QUrl
 import tkinter
 from tkinter import filedialog
 import os
+
+class Audio:
+    def __init__(self):
+        self.output = QAudioOutput()
+        self.player = QMediaPlayer()
+        self.player.setAudioOutput(self.output)
+
+        self.output.setVolume(0.9)
+
+    def play(self, path):
+        if not path:
+            return
+        
+        self.player.stop()              
+        self.player.setSource(QUrl.fromLocalFile(path))
+        self.player.play()
+        print("Playing:", path)   
 
 class Pad(QPushButton):
     def __init__(self, number = None, path = None, parent = None):
         self.path = path
         self.pad_num = number
         super().__init__(parent)
+
+        ## audi player
+        self.audio = Audio()
+        
         self.setFixedSize(150, 150) ## panna drumpad style style sisse
         self.setStyleSheet("""    
             QPushButton {
@@ -26,6 +49,11 @@ class Pad(QPushButton):
         if self.pad_num is not None:
             self.setText(f"Pad {self.pad_num}")
 
+        self.clicked.connect(self.playAudio)
+        
+    def playAudio(self):
+        self.audio.play(self.path)
+
     def contextMenuEvent(self, a0): ## a0 -> event if its not working
         menu = QMenu(self)
         pad_modify = menu.addAction("Modify")
@@ -38,7 +66,7 @@ class Pad(QPushButton):
     def modifyMusicFile(self):
         tkinter.Tk().withdraw()
         self.path = filedialog.askopenfilename(filetypes=(("Audio Files", ".wav .mp3"),   ("All Files", "*.*")))
-        if self.path is not "":
+        if self.path != "":
             file_name = os.path.split(self.path)[1]
             self.setText(file_name)
     def resetMusicFile(self):
